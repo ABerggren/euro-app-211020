8d1b1e770362456e4eaf26f071270ed543b20bff
Alexander Berggren <72595187+ABerggren@users.noreply.github.com>
2021-10-20 20:16:17 +0200

    Add files via upload

---
+++ src/containers/customerAccessPage/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import { AccountBox } from "../../components/accountBox";
+import { Footer } from "../../components/footer";
+import { Navbar } from "../../components/navbar";
+import {
+  InnerPageContainer,
+  PageContainer,
+} from "../../components/pageContainer";
+
+import { useParams } from "react-router-dom";
+
+const StyledInnerContainer = styled(InnerPageContainer)`
+  margin-top: 4em;
+`;
+
+export function CustomerAccessPage(props) {
+  const { action, userName } = useParams();
+
+  console.log(props);
+  console.log(userName);
+
+  return (
+    <PageContainer>
+      <Navbar />
+      <StyledInnerContainer>
+        <AccountBox initialActive={action} />
+      </StyledInnerContainer>
+      <Footer />
+    </PageContainer>
+  );
+}
